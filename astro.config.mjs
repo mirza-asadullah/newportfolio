d845aae652de17a1305a8e0e823ecc39f6301f8b
Mirza Asad Ullah <117911933+asad-ullah2723@users.noreply.github.com>
2026-06-01 11:14:55 +0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto',
